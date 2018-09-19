d7cf97b3d0c625c881ecbcc9916183314e1ecd6a
colin <15170933023@163.com>
2018-09-19 18:35:08 +0800

    add war pulg

--- src/main/java/com/colin/ctravel/CtravelApplication.java
+++ src/main/java/com/colin/ctravel/CtravelApplication.java
@@ -2,13 +2,24 @@ package com.colin.ctravel;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
+//@SpringBootConfiguration
 @MapperScan("com.colin.ctravel.dao")
-public class CtravelApplication {
+public class CtravelApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(CtravelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CtravelApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CtravelApplication.class);
+    }
+
 }
